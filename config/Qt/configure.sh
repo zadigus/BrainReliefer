ebefbbd8e211a3128ae6a9135bbc7c1c4747a407
zadigus <laurent1984@gmail.com>
2016-12-31 18:07:04 +0100

    give final x86_64 configuration that works

--- config/Qt/configure.sh
+++ config/Qt/configure.sh
@@ -1,9 +1,5 @@
 # desktop linux static
 ./configure --prefix="/home/mihl/Libraries/Qt/5.7/gcc_64-debug-static" -largefile -developer-build -optimized-tools -opensource -confirm-license -static -qml-debug -android-sdk /home/mihl/Libraries/android-sdk -android-ndk /home/mihl/Libraries/android-sdk/ndk-bundle -qt-xcb -no-compile-examples -nomake examples -nomake tests -android-arch armeabi-v7a -opengl -fontconfig
 
-# android x86_64
-# This doesn't work:
-#./configure --prefix="/home/mihl/Libraries/Qt/5.7/android_x86-debug-static" -static -debug -xplatform android-g++ -android-arch x86_64 -android-sdk /home/mihl/Libraries/android-sdk -android-ndk /home/mihl/Libraries/android-sdk/ndk-bundle -skip qttranslations -skip qtwebkit -skip qtserialport -skip qtwebkit-examples -qml-debug -largefile -developer-build -optimized-tools -opensource -confirm-license -qt-xcb -no-compile-examples -nomake examples -nomake tests -opengl -fontconfig
-
 # This may work:
-./configure --prefix="/home/mihl/Libraries/Qt/5.7/android_x86-debug-static" -static -debug -xplatform android-g++ -android-arch x86_64 -android-sdk /home/mihl/Libraries/android-sdk -android-ndk /home/mihl/Libraries/android-sdk/ndk-bundle -skip qttranslations -skip qtserialport -qml-debug -largefile -developer-build -optimized-tools -opensource -confirm-license -no-compile-examples -nomake examples -nomake tests -opengl -fontconfig -pkg-config -android-ndk-platform android-24
+./configure --prefix="/home/mihl/Libraries/Qt/5.7/android_x86_64-debug-static" -static -debug -xplatform android-g++ -android-arch x86_64 -android-sdk /home/mihl/Libraries/android-sdk -android-ndk /home/mihl/Libraries/android-sdk/ndk-bundle -android-ndk-platform android-24 -skip qttranslations -skip qtserialport -skip qtvirtualkeyboard -qml-debug -largefile -developer-build -optimized-tools -opensource -confirm-license -opengl -pkg-config -no-compile-examples -make libs -no-dbus
