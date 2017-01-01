fa24ea9fd7792243c4f10d968b5f92b838cb64f7
zadigus <laurent1984@gmail.com>
2017-01-01 13:36:09 +0100

    added initial almost working xerces-c configuration

---
+++ config/xerces-c/configure.sh
@@ -0,0 +1,2 @@
+# the option --with-sysroot= doesn't work!!!
+./configure --prefix=/home/mihl/Libraries/xerces-c-arm-linux-androideabi --host=arm-linux-androideabi --target=arm-linux-androideabi CPPFLAGS="--sysroot $NDK_ROOT/platforms/android-24/arch-arm"
